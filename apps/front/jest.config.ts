e22049e3062ddf06509540cbc90b15aa1205653f
Antoine Laborderie <antoine.laborderie@gmail.com>
2025-08-03 22:47:36 +0200

    :broom: fixed front angular app path

--- apps/front/jest.config.ts
+++ apps/front/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'front',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/front',
+  coverageDirectory: '../../coverage/apps/front',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
