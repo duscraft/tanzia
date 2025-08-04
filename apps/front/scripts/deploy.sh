b52d2cc826dc34cc459b25c7341d05e1f2256a6a
Antoine LABORDERIE <antoine.laborderie@gmail.com>
2025-08-04 17:09:09 +0200

    :bug: do not fail on artifact missing (if front not affected we wont have dist) + fix front deploy script

--- apps/front/scripts/deploy.sh
+++ apps/front/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-docker build . --file Dockerfile --tag tanzia-front
+docker build . --file apps/front/Dockerfile --tag tanzia-front
 docker login ghcr.io -u $GITHUB_ACTOR -p $PAT_TOKEN
 docker tag tanzia-front ghcr.io/$REPO_OWNER/tanzia-front:latest
 docker push ghcr.io/$REPO_OWNER/tanzia-front:latest
--- apps/go/scripts/deploy.sh
+++ apps/go/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-docker build . --file Dockerfile --tag tanzia
+docker build . --file apps/go/Dockerfile --tag tanzia
 docker login ghcr.io -u $GITHUB_ACTOR -p $PAT_TOKEN
 docker tag tanzia ghcr.io/$REPO_OWNER/tanzia:latest
 docker push ghcr.io/$REPO_OWNER/tanzia:latest
